8d8444aceb4c530902d2e924ae886a17ad4cdf06
nanea808 <willers.enw@gmail.com>
2023-04-04 01:31:28 -0700

    added TODO's to server

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,33 +1,37 @@
 const { gql } = require('apollo-server-express');
 
+// TODO: Implement typeDefs
+
 const typeDefs = gql`
-  type Thought {
-    _id: ID
-    thoughtText: String
-    thoughtAuthor: String
-    createdAt: String
-    comments: [Comment]!
-  }
-
-  type Comment {
-    _id: ID
-    commentText: String
-    createdAt: String
-  }
-
-  type Query {
-    thoughts: [Thought]!
-    thought(thoughtId: ID!): Thought
-  }
-
-  type Mutation {
-    addThought(thoughtText: String!, thoughtAuthor: String!): Thought
-    addComment(thoughtId: ID!, commentText: String!): Thought
-    removeThought(thoughtId: ID!): Thought
-    removeComment(thoughtId: ID!, commentId: ID!): Thought
-  }
-
   
 `;
+
+// const typeDefs = gql`
+//   type Thought {
+//     _id: ID
+//     thoughtText: String
+//     thoughtAuthor: String
+//     createdAt: String
+//     comments: [Comment]!
+//   }
+
+//   type Comment {
+//     _id: ID
+//     commentText: String
+//     createdAt: String
+//   }
+
+//   type Query {
+//     thoughts: [Thought]!
+//     thought(thoughtId: ID!): Thought
+//   }
+
+//   type Mutation {
+//     addThought(thoughtText: String!, thoughtAuthor: String!): Thought
+//     addComment(thoughtId: ID!, commentText: String!): Thought
+//     removeThought(thoughtId: ID!): Thought
+//     removeComment(thoughtId: ID!, commentId: ID!): Thought
+//   }
+// `;
 
 module.exports = typeDefs;
